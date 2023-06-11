9bfb432c38362721389cd4f54d8c10d226d3c0d8
Alan Wu <308725626@qq.com>
2023-06-11 13:08:54 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,43 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(
+  readFileSync("./package.json", { encoding: "utf-8" })
+);
+
+const globals = {
+  ...defineConfig(packageJson?.dependencies || {}),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist",
     lib: {
-      entry: "./src/components/index.ts",
+      entry: resolve(__dirname, "./src/components/index.ts"),
       name: "aw-ui",
+      formats: ["cjs", "es", "umd"],
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "react-dom", ...Object.keys(globals)],
       output: {
         globals: {
           react: "React",
         },
       },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
     },
   },
 });
